5f0db4ecfbebe53139a8c658d58a9db12521c117
janus_wel <janus.wel.3@gmail.com>
2017-11-14 15:16:13 +0900

    add kocha

---
+++ test.js
@@ -0,0 +1,13 @@
+import {
+    describe,
+    it,
+} from 'kocha'
+import assert from 'assert'
+
+import { concat } from './index'
+
+describe('concat', () => {
+    it('returns concatenated string', () => {
+        assert('1footrue' === concat([1, 'foo', true]))
+    })
+})
