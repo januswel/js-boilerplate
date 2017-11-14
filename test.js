0870acec32e768768081241f1c36a127ac64c6b2
janus_wel <janus.wel.3@gmail.com>
2017-11-14 17:09:27 +0900

    add prettier

--- test.js
+++ test.js
@@ -1,15 +1,12 @@
 // flow
 
-import {
-    describe,
-    it,
-} from 'kocha'
+import { describe, it } from 'kocha'
 import assert from 'assert'
 
 import { concat } from './index'
 
 describe('concat', () => {
-    it('returns concatenated string', () => {
-        assert('1footrue' === concat(1, 'foo', true))
-    })
+  it('returns concatenated string', () => {
+    assert('1footrue' === concat(1, 'foo', true))
+  })
 })
